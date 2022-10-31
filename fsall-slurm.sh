d17bf93e3c7fa54729e47d10134c387edfc8f19b
AWS ParallelCluster user <ec2-user@ip-10-0-13-220.ap-southeast-1.compute.internal>
2022-10-31 03:10:16 +0000

    New stuff

--- fsall-slurm.sh
+++ fsall-slurm.sh
@@ -1,12 +1,15 @@
+#!/bin/bash
+python -u -c "import PyHipp as pyh; \
 import DataProcessingTools as DPT; \
 lfall = DPT.objects.processDirs(dirs=None, exclude=['*eye*', '*mountains*'], objtype=pyh.FreqSpectrum, saveLevel=1); \
 lfall.save(); \
 hfall = DPT.objects.processDirs(dirs=None, exclude=['*eye*', '*mountains*'], objtype=pyh.FreqSpectrum, loadHighPass=True, pointsPerWindow=3000, saveLevel=1); \
-hfall.save();
+hfall.save(); \
 
-python -u -c "import PyHipp as pyh; \
 import time; \
 pyh.RPLLFP(saveLevel=1); \
 print(time.localtime());"
 
 aws sns publish --topic-arn arn:aws:sns:ap-southeast-1:244260284409:awsnotify --message "FSJobDone"
+
+

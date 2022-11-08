ba6cb9734e3bec1a277850eac9f624e5b65ed543
AWS ParallelCluster user <ec2-user@ip-10-0-12-69.ap-southeast-1.compute.internal>
2022-11-08 08:16:41 +0000

    lab 6 files

--- rse-slurm.sh
+++ rse-slurm.sh
@@ -1,3 +1,18 @@
+#!/bin/bash
+
+# Submit this script with: sbatch <this-filename>
+
+#SBATCH --time=1:00:00   # walltime
+#SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH -J "rse-job"   # job name
+
+## /SBATCH -p general # partition (queue)
+#SBATCH -o rse-slurm.%N.%j.out # STDOUT
+#SBATCH -e rse-slurm.%N.%j.err # STDERR
+
+# LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+
 python -u -c "import PyHipp as pyh; \
 import time; \
 import os; \
